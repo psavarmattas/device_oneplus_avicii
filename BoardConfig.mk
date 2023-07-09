08b69e981a0ea9bcdbe60b87db69cc92dbb8a500
psavarmattas <puranjaysavarmattas@gmail.com>
2023-07-09 10:18:04 +0530

    avicii: Set SELINUX_IGNORE_NEVERALLOWS to true.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,7 @@ VENDOR_SECURITY_PATCH := 2023-03-05
 # SEPolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Touch
 SOONG_CONFIG_NAMESPACES += OPLUS_LINEAGE_TOUCH_HAL
